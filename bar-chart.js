479a485fb139a80bd74abaa9797713518e2ead15
Tech Squad <techsquad@digitalnest.org>
2018-07-12 13:23:13 -0700

    Part3- Update2

--- bar-chart.js
+++ bar-chart.js
@@ -1,13 +1,23 @@
 var sessionArray = [1, 2, 3]
-var timestampArray = [30, 45, 12000]
+var timestampArray = [30, 45, 750]
 
 //Creates the svg
 var svg = d3.select(".bar-chart").append("svg").attr("height","100%").attr("width", "100%");
 
+//Making the y axis
+var y = d3.scaleLinear()
+        .domain([0, 750])
+        .range([150, 0]);
+
+var yAxis = d3.axisLeft(y);
+
+//Making rectangles
 svg.selectAll("rect")
   .data(timestampArray)
   .enter().append("rect")
   .attr("height",function(d,i){return d;})
   .attr("width","50")
   .attr("x",function(d,i){return 60*i;})
-  .attr("y","0");
+  .attr("y",function(d,i){return 150 -(d);});
+
+svg.append("g").attr("class", "axis y").call(yAxis);
